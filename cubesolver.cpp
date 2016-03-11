68945473bc17fb3010056666fd6a287e8f97eb9b
timothymachasio <tmachasi@nd.edu>
2016-03-10 21:26:36 -0500

    Update cubesolver.cpp

--- cubesolver.cpp
+++ cubesolver.cpp
@@ -1,1 +1,474 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void R(void);
+void L(void);
+void U(void);
+void D(void);
+void F(void);
+void B(void);
+
+void algorithm(void);
+bool IsSolved(void);
+
+int solvedmatrix[6][9] = {{0,0,0,0,0,0,0,0,0},{1,1,1,1,1,1,1,1,1},{2,2,2,2,2,2,2,2,2},{3,3,3,3,3,3,3,3,3},{4,4,4,4,4,4,4,4,4},{5,5,5,5,5,5,5,5,5}};
+int unsolvedmatrix[6][9] = {{0,0,0,0,0,0,0,0,0},{1,1,1,1,1,1,1,1,1},{2,2,2,2,2,2,2,2,2},{3,3,3,3,3,3,3,3,3},{4,4,4,4,4,4,4,4,4},{5,5,5,5,5,5,5,5,5}};
+int workingmatrix[6][9];
+
+int main() {
+    
+    ofstream fout("matrix.dat");
+    
+    cout<<"State of cube before algorithm: "<<endl;
+    
+    for (int i=0;i<6;i++) {
+        for (int j=0;j<9;j++) {
+            cout<<unsolvedmatrix[i][j];
+        }
+        cout<<endl;
+    }
+    
+    //TEST: Combination of moves defined in void algorithm()(See function definition)
+    
+    algorithm();
+    
+    cout<<"State of cube after algorithm: "<<endl;
+    
+    for (int i=0;i<6;i++) {
+        for (int j=0;j<9;j++) {
+            cout<<unsolvedmatrix[i][j];
+        }
+        cout<<endl;
+    }
+    
+    
+    
+    cout<<"The cube is ";
+    if (!IsSolved()) {
+        cout<<"not";
+    }
+    cout<<" solved."<<endl;
+    
+    //TEST: How many times the algorithm will run before the cube is solved
+    int reps;
+    
+    while (!IsSolved()) {
+        
+        algorithm();
+        reps++;
+    }
+    cout<<"The cube will be solved after the algorithm is repeated "<<reps+1<<" times."<<endl;
+    
+    return 0;
+}
+
+
+
+
+void R() {
+    workingmatrix[0][0]=unsolvedmatrix[0][0];
+    workingmatrix[0][1]=unsolvedmatrix[0][1];
+    workingmatrix[0][2]=unsolvedmatrix[3][2];
+    workingmatrix[0][3]=unsolvedmatrix[0][3];
+    workingmatrix[0][4]=unsolvedmatrix[0][4];
+    workingmatrix[0][5]=unsolvedmatrix[3][5];
+    workingmatrix[0][6]=unsolvedmatrix[0][6];
+    workingmatrix[0][7]=unsolvedmatrix[0][7];
+    workingmatrix[0][8]=unsolvedmatrix[3][8];
+    workingmatrix[1][0]=unsolvedmatrix[1][0];
+    workingmatrix[1][1]=unsolvedmatrix[1][1];
+    workingmatrix[1][2]=unsolvedmatrix[0][2];
+    workingmatrix[1][3]=unsolvedmatrix[1][3];
+    workingmatrix[1][4]=unsolvedmatrix[1][4];
+    workingmatrix[1][5]=unsolvedmatrix[0][5];
+    workingmatrix[1][6]=unsolvedmatrix[1][6];
+    workingmatrix[1][7]=unsolvedmatrix[1][7];
+    workingmatrix[1][8]=unsolvedmatrix[0][8];
+    workingmatrix[2][0]=unsolvedmatrix[2][0];
+    workingmatrix[2][1]=unsolvedmatrix[2][1];
+    workingmatrix[2][2]=unsolvedmatrix[1][2];
+    workingmatrix[2][3]=unsolvedmatrix[2][3];
+    workingmatrix[2][4]=unsolvedmatrix[2][4];
+    workingmatrix[2][5]=unsolvedmatrix[1][5];
+    workingmatrix[2][6]=unsolvedmatrix[2][6];
+    workingmatrix[2][7]=unsolvedmatrix[2][7];
+    workingmatrix[2][8]=unsolvedmatrix[1][8];
+    workingmatrix[3][0]=unsolvedmatrix[3][0];
+    workingmatrix[3][1]=unsolvedmatrix[3][1];
+    workingmatrix[3][2]=unsolvedmatrix[2][2];
+    workingmatrix[3][3]=unsolvedmatrix[3][3];
+    workingmatrix[3][4]=unsolvedmatrix[3][4];
+    workingmatrix[3][5]=unsolvedmatrix[2][5];
+    workingmatrix[3][6]=unsolvedmatrix[3][6];
+    workingmatrix[3][7]=unsolvedmatrix[3][7];
+    workingmatrix[3][8]=unsolvedmatrix[2][8];
+    workingmatrix[4][0]=unsolvedmatrix[4][6];
+    workingmatrix[4][1]=unsolvedmatrix[4][3];
+    workingmatrix[4][2]=unsolvedmatrix[4][0];
+    workingmatrix[4][3]=unsolvedmatrix[4][7];
+    workingmatrix[4][4]=unsolvedmatrix[4][4];
+    workingmatrix[4][5]=unsolvedmatrix[4][1];
+    workingmatrix[4][6]=unsolvedmatrix[4][8];
+    workingmatrix[4][7]=unsolvedmatrix[4][5];
+    workingmatrix[4][8]=unsolvedmatrix[4][2];
+    workingmatrix[5][0]=unsolvedmatrix[5][0];
+    workingmatrix[5][1]=unsolvedmatrix[5][1];
+    workingmatrix[5][2]=unsolvedmatrix[5][2];
+    workingmatrix[5][3]=unsolvedmatrix[5][3];
+    workingmatrix[5][4]=unsolvedmatrix[5][4];
+    workingmatrix[5][5]=unsolvedmatrix[5][5];
+    workingmatrix[5][6]=unsolvedmatrix[5][6];
+    workingmatrix[5][7]=unsolvedmatrix[5][7];
+    workingmatrix[5][8]=unsolvedmatrix[5][8];
+    
+    
+    for (int i=0;i<6;i++) {
+        for (int j=0;j<9;j++) {
+            unsolvedmatrix[i][j]=workingmatrix[i][j];
+        }
+        cout<<endl;
+    }
+};
+
+
+void L() {
+    workingmatrix[0][0]=unsolvedmatrix[1][0];
+    workingmatrix[0][1]=unsolvedmatrix[0][1];
+    workingmatrix[0][2]=unsolvedmatrix[0][2];
+    workingmatrix[0][3]=unsolvedmatrix[1][3];
+    workingmatrix[0][4]=unsolvedmatrix[0][4];
+    workingmatrix[0][5]=unsolvedmatrix[0][5];
+    workingmatrix[0][6]=unsolvedmatrix[1][6];
+    workingmatrix[0][7]=unsolvedmatrix[0][7];
+    workingmatrix[0][8]=unsolvedmatrix[0][8];
+    workingmatrix[1][0]=unsolvedmatrix[2][0];
+    workingmatrix[1][1]=unsolvedmatrix[1][1];
+    workingmatrix[1][2]=unsolvedmatrix[1][2];
+    workingmatrix[1][3]=unsolvedmatrix[2][3];
+    workingmatrix[1][4]=unsolvedmatrix[1][4];
+    workingmatrix[1][5]=unsolvedmatrix[1][5];
+    workingmatrix[1][6]=unsolvedmatrix[2][6];
+    workingmatrix[1][7]=unsolvedmatrix[1][7];
+    workingmatrix[1][8]=unsolvedmatrix[1][8];
+    workingmatrix[2][0]=unsolvedmatrix[3][0];
+    workingmatrix[2][1]=unsolvedmatrix[2][1];
+    workingmatrix[2][2]=unsolvedmatrix[2][2];
+    workingmatrix[2][3]=unsolvedmatrix[3][3];
+    workingmatrix[2][4]=unsolvedmatrix[2][4];
+    workingmatrix[2][5]=unsolvedmatrix[2][5];
+    workingmatrix[2][6]=unsolvedmatrix[3][6];
+    workingmatrix[2][7]=unsolvedmatrix[2][7];
+    workingmatrix[2][8]=unsolvedmatrix[2][8];
+    workingmatrix[3][0]=unsolvedmatrix[0][0];
+    workingmatrix[3][1]=unsolvedmatrix[3][1];
+    workingmatrix[3][2]=unsolvedmatrix[3][2];
+    workingmatrix[3][3]=unsolvedmatrix[0][3];
+    workingmatrix[3][4]=unsolvedmatrix[3][4];
+    workingmatrix[3][5]=unsolvedmatrix[3][5];
+    workingmatrix[3][6]=unsolvedmatrix[0][6];
+    workingmatrix[3][7]=unsolvedmatrix[3][7];
+    workingmatrix[3][8]=unsolvedmatrix[3][8];
+    workingmatrix[4][0]=unsolvedmatrix[4][0];
+    workingmatrix[4][1]=unsolvedmatrix[4][1];
+    workingmatrix[4][2]=unsolvedmatrix[4][2];
+    workingmatrix[4][3]=unsolvedmatrix[4][3];
+    workingmatrix[4][4]=unsolvedmatrix[4][4];
+    workingmatrix[4][5]=unsolvedmatrix[4][5];
+    workingmatrix[4][6]=unsolvedmatrix[4][6];
+    workingmatrix[4][7]=unsolvedmatrix[4][7];
+    workingmatrix[4][8]=unsolvedmatrix[4][8];
+    workingmatrix[5][0]=unsolvedmatrix[5][6];
+    workingmatrix[5][1]=unsolvedmatrix[5][3];
+    workingmatrix[5][2]=unsolvedmatrix[5][0];
+    workingmatrix[5][3]=unsolvedmatrix[5][7];
+    workingmatrix[5][4]=unsolvedmatrix[5][4];
+    workingmatrix[5][5]=unsolvedmatrix[5][1];
+    workingmatrix[5][6]=unsolvedmatrix[5][8];
+    workingmatrix[5][7]=unsolvedmatrix[5][5];
+    workingmatrix[5][8]=unsolvedmatrix[5][2];
+    
+    for (int i=0;i<6;i++) {
+        for (int j=0;j<9;j++) {
+            unsolvedmatrix[i][j]=workingmatrix[i][j];
+        }
+        cout<<endl;
+    }
+};
+
+void U() {
+    workingmatrix[0][0]=unsolvedmatrix[0][0];
+    workingmatrix[0][1]=unsolvedmatrix[0][1];
+    workingmatrix[0][2]=unsolvedmatrix[0][2];
+    workingmatrix[0][3]=unsolvedmatrix[0][3];
+    workingmatrix[0][4]=unsolvedmatrix[0][4];
+    workingmatrix[0][5]=unsolvedmatrix[0][5];
+    workingmatrix[0][6]=unsolvedmatrix[0][6];
+    workingmatrix[0][7]=unsolvedmatrix[0][7];
+    workingmatrix[0][8]=unsolvedmatrix[0][8];
+    workingmatrix[1][0]=unsolvedmatrix[4][0];
+    workingmatrix[1][1]=unsolvedmatrix[4][1];
+    workingmatrix[1][2]=unsolvedmatrix[4][2];
+    workingmatrix[1][3]=unsolvedmatrix[1][3];
+    workingmatrix[1][4]=unsolvedmatrix[1][4];
+    workingmatrix[1][5]=unsolvedmatrix[1][5];
+    workingmatrix[1][6]=unsolvedmatrix[1][6];
+    workingmatrix[1][7]=unsolvedmatrix[1][7];
+    workingmatrix[1][8]=unsolvedmatrix[1][8];
+    workingmatrix[2][0]=unsolvedmatrix[2][6];
+    workingmatrix[2][1]=unsolvedmatrix[2][3];
+    workingmatrix[2][2]=unsolvedmatrix[2][0];
+    workingmatrix[2][3]=unsolvedmatrix[2][7];
+    workingmatrix[2][4]=unsolvedmatrix[2][4];
+    workingmatrix[2][5]=unsolvedmatrix[2][1];
+    workingmatrix[2][6]=unsolvedmatrix[2][8];
+    workingmatrix[2][7]=unsolvedmatrix[2][5];
+    workingmatrix[2][8]=unsolvedmatrix[2][2];
+    workingmatrix[3][0]=unsolvedmatrix[3][0];
+    workingmatrix[3][1]=unsolvedmatrix[3][1];
+    workingmatrix[3][2]=unsolvedmatrix[3][2];
+    workingmatrix[3][3]=unsolvedmatrix[3][3];
+    workingmatrix[3][4]=unsolvedmatrix[3][4];
+    workingmatrix[3][5]=unsolvedmatrix[3][5];
+    workingmatrix[3][6]=unsolvedmatrix[5][2];
+    workingmatrix[3][7]=unsolvedmatrix[5][1];
+    workingmatrix[3][8]=unsolvedmatrix[5][0];
+    workingmatrix[4][0]=unsolvedmatrix[3][8];
+    workingmatrix[4][1]=unsolvedmatrix[3][7];
+    workingmatrix[4][2]=unsolvedmatrix[3][6];
+    workingmatrix[4][3]=unsolvedmatrix[4][3];
+    workingmatrix[4][4]=unsolvedmatrix[4][4];
+    workingmatrix[4][5]=unsolvedmatrix[4][5];
+    workingmatrix[4][6]=unsolvedmatrix[4][6];
+    workingmatrix[4][7]=unsolvedmatrix[4][7];
+    workingmatrix[4][8]=unsolvedmatrix[4][8];
+    workingmatrix[5][0]=unsolvedmatrix[1][0];
+    workingmatrix[5][1]=unsolvedmatrix[1][1];
+    workingmatrix[5][2]=unsolvedmatrix[1][2];
+    workingmatrix[5][3]=unsolvedmatrix[5][3];
+    workingmatrix[5][4]=unsolvedmatrix[5][4];
+    workingmatrix[5][5]=unsolvedmatrix[5][5];
+    workingmatrix[5][6]=unsolvedmatrix[5][6];
+    workingmatrix[5][7]=unsolvedmatrix[5][7];
+    workingmatrix[5][8]=unsolvedmatrix[5][8];
+    
+    for (int i=0;i<6;i++) {
+        for (int j=0;j<9;j++) {
+            unsolvedmatrix[i][j]=workingmatrix[i][j];
+        }
+        cout<<endl;
+    }
+};
+
+void D() {
+    workingmatrix[0][0]=unsolvedmatrix[0][6];
+    workingmatrix[0][1]=unsolvedmatrix[0][3];
+    workingmatrix[0][2]=unsolvedmatrix[0][0];
+    workingmatrix[0][3]=unsolvedmatrix[0][7];
+    workingmatrix[0][4]=unsolvedmatrix[0][4];
+    workingmatrix[0][5]=unsolvedmatrix[0][1];
+    workingmatrix[0][6]=unsolvedmatrix[0][8];
+    workingmatrix[0][7]=unsolvedmatrix[0][5];
+    workingmatrix[0][8]=unsolvedmatrix[0][2];
+    workingmatrix[1][0]=unsolvedmatrix[1][0];
+    workingmatrix[1][1]=unsolvedmatrix[1][1];
+    workingmatrix[1][2]=unsolvedmatrix[1][2];
+    workingmatrix[1][3]=unsolvedmatrix[1][3];
+    workingmatrix[1][4]=unsolvedmatrix[1][4];
+    workingmatrix[1][5]=unsolvedmatrix[1][5];
+    workingmatrix[1][6]=unsolvedmatrix[5][6];
+    workingmatrix[1][7]=unsolvedmatrix[5][7];
+    workingmatrix[1][8]=unsolvedmatrix[5][8];
+    workingmatrix[2][0]=unsolvedmatrix[2][0];
+    workingmatrix[2][1]=unsolvedmatrix[2][1];
+    workingmatrix[2][2]=unsolvedmatrix[2][2];
+    workingmatrix[2][3]=unsolvedmatrix[2][3];
+    workingmatrix[2][4]=unsolvedmatrix[2][4];
+    workingmatrix[2][5]=unsolvedmatrix[2][5];
+    workingmatrix[2][6]=unsolvedmatrix[2][6];
+    workingmatrix[2][7]=unsolvedmatrix[2][7];
+    workingmatrix[2][8]=unsolvedmatrix[2][8];
+    workingmatrix[3][0]=unsolvedmatrix[4][8];
+    workingmatrix[3][1]=unsolvedmatrix[4][7];
+    workingmatrix[3][2]=unsolvedmatrix[4][6];
+    workingmatrix[3][3]=unsolvedmatrix[3][3];
+    workingmatrix[3][4]=unsolvedmatrix[3][4];
+    workingmatrix[3][5]=unsolvedmatrix[3][5];
+    workingmatrix[3][6]=unsolvedmatrix[3][6];
+    workingmatrix[3][7]=unsolvedmatrix[3][7];
+    workingmatrix[3][8]=unsolvedmatrix[3][8];
+    workingmatrix[4][0]=unsolvedmatrix[4][0];
+    workingmatrix[4][1]=unsolvedmatrix[4][1];
+    workingmatrix[4][2]=unsolvedmatrix[4][2];
+    workingmatrix[4][3]=unsolvedmatrix[4][3];
+    workingmatrix[4][4]=unsolvedmatrix[4][4];
+    workingmatrix[4][5]=unsolvedmatrix[4][5];
+    workingmatrix[4][6]=unsolvedmatrix[1][6];
+    workingmatrix[4][7]=unsolvedmatrix[1][7];
+    workingmatrix[4][8]=unsolvedmatrix[1][8];
+    workingmatrix[5][0]=unsolvedmatrix[5][0];
+    workingmatrix[5][1]=unsolvedmatrix[5][1];
+    workingmatrix[5][2]=unsolvedmatrix[5][2];
+    workingmatrix[5][3]=unsolvedmatrix[5][3];
+    workingmatrix[5][4]=unsolvedmatrix[5][4];
+    workingmatrix[5][5]=unsolvedmatrix[5][5];
+    workingmatrix[5][6]=unsolvedmatrix[3][2];
+    workingmatrix[5][7]=unsolvedmatrix[3][1];
+    workingmatrix[5][8]=unsolvedmatrix[3][0];
+    
+    for (int i=0;i<6;i++) {
+        for (int j=0;j<9;j++) {
+            unsolvedmatrix[i][j]=workingmatrix[i][j];
+        }
+        cout<<endl;
+    }
+};
+
+void F() {
+    workingmatrix[0][0]=unsolvedmatrix[4][6];
+    workingmatrix[0][1]=unsolvedmatrix[4][3];
+    workingmatrix[0][2]=unsolvedmatrix[4][0];
+    workingmatrix[0][3]=unsolvedmatrix[0][3];
+    workingmatrix[0][4]=unsolvedmatrix[0][4];
+    workingmatrix[0][5]=unsolvedmatrix[0][5];
+    workingmatrix[0][6]=unsolvedmatrix[0][6];
+    workingmatrix[0][7]=unsolvedmatrix[0][7];
+    workingmatrix[0][8]=unsolvedmatrix[0][8];
+    workingmatrix[1][0]=unsolvedmatrix[1][6];
+    workingmatrix[1][1]=unsolvedmatrix[1][3];
+    workingmatrix[1][2]=unsolvedmatrix[1][0];
+    workingmatrix[1][3]=unsolvedmatrix[1][7];
+    workingmatrix[1][4]=unsolvedmatrix[1][4];
+    workingmatrix[1][5]=unsolvedmatrix[1][1];
+    workingmatrix[1][6]=unsolvedmatrix[1][8];
+    workingmatrix[1][7]=unsolvedmatrix[1][5];
+    workingmatrix[1][8]=unsolvedmatrix[1][2];
+    workingmatrix[2][0]=unsolvedmatrix[2][0];
+    workingmatrix[2][1]=unsolvedmatrix[2][1];
+    workingmatrix[2][2]=unsolvedmatrix[2][2];
+    workingmatrix[2][3]=unsolvedmatrix[2][3];
+    workingmatrix[2][4]=unsolvedmatrix[2][4];
+    workingmatrix[2][5]=unsolvedmatrix[2][5];
+    workingmatrix[2][6]=unsolvedmatrix[5][8];
+    workingmatrix[2][7]=unsolvedmatrix[5][5];
+    workingmatrix[2][8]=unsolvedmatrix[5][2];
+    workingmatrix[3][0]=unsolvedmatrix[3][0];
+    workingmatrix[3][1]=unsolvedmatrix[3][1];
+    workingmatrix[3][2]=unsolvedmatrix[3][2];
+    workingmatrix[3][3]=unsolvedmatrix[3][3];
+    workingmatrix[3][4]=unsolvedmatrix[3][4];
+    workingmatrix[3][5]=unsolvedmatrix[3][5];
+    workingmatrix[3][6]=unsolvedmatrix[3][6];
+    workingmatrix[3][7]=unsolvedmatrix[3][7];
+    workingmatrix[3][8]=unsolvedmatrix[3][8];
+    workingmatrix[4][0]=unsolvedmatrix[2][6];
+    workingmatrix[4][1]=unsolvedmatrix[4][1];
+    workingmatrix[4][2]=unsolvedmatrix[4][2];
+    workingmatrix[4][3]=unsolvedmatrix[2][7];
+    workingmatrix[4][4]=unsolvedmatrix[4][4];
+    workingmatrix[4][5]=unsolvedmatrix[4][5];
+    workingmatrix[4][6]=unsolvedmatrix[2][8];
+    workingmatrix[4][7]=unsolvedmatrix[4][7];
+    workingmatrix[4][8]=unsolvedmatrix[4][8];
+    workingmatrix[5][0]=unsolvedmatrix[5][0];
+    workingmatrix[5][1]=unsolvedmatrix[5][1];
+    workingmatrix[5][2]=unsolvedmatrix[0][0];
+    workingmatrix[5][3]=unsolvedmatrix[5][3];
+    workingmatrix[5][4]=unsolvedmatrix[5][4];
+    workingmatrix[5][5]=unsolvedmatrix[0][1];
+    workingmatrix[5][6]=unsolvedmatrix[5][6];
+    workingmatrix[5][7]=unsolvedmatrix[5][7];
+    workingmatrix[5][8]=unsolvedmatrix[0][2];
+    
+    for (int i=0;i<6;i++) {
+        for (int j=0;j<9;j++) {
+            unsolvedmatrix[i][j]=workingmatrix[i][j];
+        }
+        cout<<endl;
+    }
+};
+
+void B() {
+    workingmatrix[0][0]=unsolvedmatrix[0][0];
+    workingmatrix[0][1]=unsolvedmatrix[0][1];
+    workingmatrix[0][2]=unsolvedmatrix[0][2];
+    workingmatrix[0][3]=unsolvedmatrix[0][3];
+    workingmatrix[0][4]=unsolvedmatrix[0][4];
+    workingmatrix[0][5]=unsolvedmatrix[0][5];
+    workingmatrix[0][6]=unsolvedmatrix[5][0];
+    workingmatrix[0][7]=unsolvedmatrix[5][3];
+    workingmatrix[0][8]=unsolvedmatrix[5][6];
+    workingmatrix[1][0]=unsolvedmatrix[1][0];
+    workingmatrix[1][1]=unsolvedmatrix[1][1];
+    workingmatrix[1][2]=unsolvedmatrix[1][2];
+    workingmatrix[1][3]=unsolvedmatrix[1][3];
+    workingmatrix[1][4]=unsolvedmatrix[1][4];
+    workingmatrix[1][5]=unsolvedmatrix[1][5];
+    workingmatrix[1][6]=unsolvedmatrix[1][6];
+    workingmatrix[1][7]=unsolvedmatrix[1][7];
+    workingmatrix[1][8]=unsolvedmatrix[1][8];
+    workingmatrix[2][0]=unsolvedmatrix[4][2];
+    workingmatrix[2][1]=unsolvedmatrix[4][5];
+    workingmatrix[2][2]=unsolvedmatrix[4][8];
+    workingmatrix[2][3]=unsolvedmatrix[2][3];
+    workingmatrix[2][4]=unsolvedmatrix[2][4];
+    workingmatrix[2][5]=unsolvedmatrix[2][5];
+    workingmatrix[2][6]=unsolvedmatrix[2][6];
+    workingmatrix[2][7]=unsolvedmatrix[2][7];
+    workingmatrix[2][8]=unsolvedmatrix[2][8];
+    workingmatrix[3][0]=unsolvedmatrix[3][6];
+    workingmatrix[3][1]=unsolvedmatrix[3][3];
+    workingmatrix[3][2]=unsolvedmatrix[3][0];
+    workingmatrix[3][3]=unsolvedmatrix[3][7];
+    workingmatrix[3][4]=unsolvedmatrix[3][4];
+    workingmatrix[3][5]=unsolvedmatrix[3][1];
+    workingmatrix[3][6]=unsolvedmatrix[3][8];
+    workingmatrix[3][7]=unsolvedmatrix[3][5];
+    workingmatrix[3][8]=unsolvedmatrix[3][2];
+    workingmatrix[4][0]=unsolvedmatrix[4][0];
+    workingmatrix[4][1]=unsolvedmatrix[4][1];
+    workingmatrix[4][2]=unsolvedmatrix[0][8];
+    workingmatrix[4][3]=unsolvedmatrix[4][3];
+    workingmatrix[4][4]=unsolvedmatrix[4][4];
+    workingmatrix[4][5]=unsolvedmatrix[0][7];
+    workingmatrix[4][6]=unsolvedmatrix[4][6];
+    workingmatrix[4][7]=unsolvedmatrix[4][7];
+    workingmatrix[4][8]=unsolvedmatrix[0][6];
+    workingmatrix[5][0]=unsolvedmatrix[2][2];
+    workingmatrix[5][1]=unsolvedmatrix[5][1];
+    workingmatrix[5][2]=unsolvedmatrix[5][2];
+    workingmatrix[5][3]=unsolvedmatrix[2][1];
+    workingmatrix[5][4]=unsolvedmatrix[5][4];
+    workingmatrix[5][5]=unsolvedmatrix[5][5];
+    workingmatrix[5][6]=unsolvedmatrix[2][0];
+    workingmatrix[5][7]=unsolvedmatrix[5][7];
+    workingmatrix[5][8]=unsolvedmatrix[5][8];
+    
+    for (int i=0;i<6;i++) {
+        for (int j=0;j<9;j++) {
+            unsolvedmatrix[i][j]=workingmatrix[i][j];
+        }
+        cout<<endl;
+    }
+};
+
+bool IsSolved() {
+    for (int i=0;i<6;i++) {
+        for (int j=0;j<9;j++) {
+            if (unsolvedmatrix[i][j]!=solvedmatrix[i][j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+void algorithm() {
+    
+    U();
+    D();
+
+};
 
